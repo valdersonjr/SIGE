43c9221606cbeca15f992243c6336ecaf03c19ef
valdersonjr <valdersonpontes@gmail.com>
2023-01-26 09:51:09 -0300

    Consertando comportamento da sidebar

--- src/routes/Gateway/Gateway.routes.tsx
+++ src/routes/Gateway/Gateway.routes.tsx
@@ -4,10 +4,18 @@ import { AuthenticatedRoutes } from '../Authenticated/Authenticated.routes';
 import { NotAuthenticatedRoutes } from '../NotAuthenticated/NotAuthenticated.routes';
 
 import { auth } from '@recoil';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
+import { selectedSidebar } from "~/recoil/sidebar/sidebar.atom";
 
 export const Gateway: React.FC = () => {
   const token = useRecoilValue(auth.atom.token);
+  const setSelectedSection = useSetRecoilState(selectedSidebar)
+
+  location.pathname.includes('/alunos') && setSelectedSection(1);
+  location.pathname.includes('/gestao-escolar') && setSelectedSection(2);
+  location.pathname.includes('/usuarios') && setSelectedSection(3);
+  location.pathname.includes('/relatorios') && setSelectedSection(4);
 
   if (!token) {
     return (
