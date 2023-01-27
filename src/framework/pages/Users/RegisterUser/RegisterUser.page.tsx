ed78b920ea6b6bfdba96352aae491096a21a7d9f
valdersonjr <valdersonpontes@gmail.com>
2023-01-27 15:06:51 -0300

    fix validacao telefone criacao usuario

--- src/framework/pages/Users/RegisterUser/RegisterUser.page.tsx
+++ src/framework/pages/Users/RegisterUser/RegisterUser.page.tsx
@@ -13,7 +13,8 @@ const RegisterUserPage: React.FC = () => {
         user.phone = user.phone.replace(")", "");
         user.phone = user.phone.replace("(", "");
 
-        if(user.profile.length === 0) alert("Atenção! \nSelecione pelo menos um perfil.");
+        if(user.phone.length !== 11) alert("Atenção \nTelefone inválido");
+        else if(user.profile.length === 0) alert("Atenção! \nSelecione pelo menos um perfil.");
         else if(user.password.length < 6) alert("Senha inválida! \nSua senha precisa ter no mínimo 6 dígitos.");
         else if(user.confirmPassword !== user.password) alert("Atenção! \nAs senhas precisam ser iguais.");
   
